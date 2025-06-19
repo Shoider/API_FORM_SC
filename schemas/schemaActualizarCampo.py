b4f4a45678b8c180aa0fdba9cf81c2c4bb511f11
BrandonHS <branditis1211@gmail.com>
2025-06-19 12:29:28 -0600

    fix(schema): Puse mal algo

--- schemas/schemaActualizarCampo.py
+++ schemas/schemaActualizarCampo.py
@@ -3,4 +3,4 @@ from marshmallow import Schema, fields, validate
 class ActualizacionCampo(Schema):
     noticket=fields.String(required=False)
     memorando=fields.String(required=False)
-    numeroFormato=fields.String(requiried=True)
+    numeroFormato=fields.String(required=True)
