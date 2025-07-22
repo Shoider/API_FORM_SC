7e224ea2b85f92dd4cec1edd559c2a3b3b85657b
BrandonHS <branditis1211@gmail.com>
2025-07-22 16:52:00 -0600

    fix(schema): Dislexia

--- schemas/schemaActualizarFuncionRol.py
+++ schemas/schemaActualizarFuncionRol.py
@@ -3,5 +3,5 @@ from marshmallow import Schema, fields, validate
 class ActualizacionFuncionRol(Schema):
     numeroFormato=fields.String(required=True)
     funcionrol=fields.String(required=True)
-    numeroRegistro=fields.String(requiried=True)
+    numeroRegistro=fields.String(required=True)
     movimientoID=fields.String(required=True)
