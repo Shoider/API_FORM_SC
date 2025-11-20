7eed4cb57db997da49dda6c9e2c9c887bcef5a2a
NidiaVGarcia <nidiavgarcia12@gmail.com>
2025-11-20 13:58:49 -0600

    fix(dns): Se agregó una validación de que debe de ser 20 caracteres en la justficación

--- schemas/schemaDNS.py
+++ schemas/schemaDNS.py
@@ -4,27 +4,27 @@ class RegistroSchemaDNS(Schema):
     class Meta:
         ordered = True
    
-    movimiento= fields.String(required=False, validate=validate.OneOf(["ALTA", "BAJA", "CAMBIO"]))
+    movimiento= fields.String(required=True, validate=validate.OneOf(["ALTA", "BAJA", "CAMBIO"]))
     
-    nombreResponsable= fields.String(required=False)
-    puestoResponsable = fields.String(required=False)
+    nombreResponsable= fields.String(required=True)
+    puestoResponsable = fields.String(required=True)
     
-    nombreUsuario= fields.String(required=False)
-    areaUsuario= fields.String(required=False)
-    puestousuario= fields.String(required=False)
-    direccionUsuario= fields.String(required=False)    
+    nombreUsuario= fields.String(required=True)
+    areaUsuario= fields.String(required=True)
+    puestousuario= fields.String(required=True)
+    direccionUsuario= fields.String(required=True)    
     ala= fields.String(required=False)
     piso= fields.String(required=False)
     telefonoUsuario= fields.String(required=False)
-    extUsuario= fields.String(required=False)
+    extUsuario= fields.String(required=True)
     
-    registro= fields.String(required=False, validate=validate.OneOf(["CNA", "CONAGUA"]))
+    registro= fields.String(required=True, validate=validate.OneOf(["CNA", "CONAGUA"]))
     nombreRegistro= fields.String(required=False)
-    IP= fields.String(required=False)
+    IP= fields.String(required=True)
     nombreAplicacion= fields.String(required=False)
-    justificacion= fields.String(required=False)
+    justificacion= fields.String(required=True,validate=validate.Length(min=20, error="La justificación debe tener mínimo 50 caracteres."))
 
     fecha= fields.String(required=False)
 
-    nombreAproba= fields.String(required=False)
-    puestoAproba= fields.String(required=False)
+    nombreAproba= fields.String(required=True)
+    puestoAproba= fields.String(required=True)
